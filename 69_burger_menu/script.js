0b3e6c796ddfa621bb3d5a57e5deb4df84ee8f43
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-04-10 06:20:57 -0300

    69_burger_menu

---
+++ 69_burger_menu/script.js
@@ -0,0 +1,6 @@
+$('.burger, .overlay').click(function(){
+  $('.burger').toggleClass('clicked');
+  $('.overlay').toggleClass('show');
+  $('nav').toggleClass('show');
+  $('body').toggleClass('overflow');
+});
