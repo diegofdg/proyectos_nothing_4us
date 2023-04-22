4eea638247b21567b0aa4de18844f89332483152
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-04-22 06:26:41 -0300

    81_3d_ecommerce_nike

---
+++ 81_3d_ecommerce_nike/script.js
@@ -0,0 +1,4 @@
+document.getElementById('canvas3d').addEventListener('click', () => {
+  document.getElementById('rotate').classList.toggle('bg-indigo-600');
+  document.getElementById('rotate').classList.toggle('bg-lime-500');
+});
