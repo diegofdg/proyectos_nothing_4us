1bb0ac62c1fa4984ad73b0ab404199cd3d196fcb
diegofdg <diegofdg@gmail.com>
2025-03-15 09:47:30 -0300

    84_landing-page-with-swiper-css-swiper-js

---
+++ 84_landing-page-with-swiper-css-swiper-js/script.js
@@ -0,0 +1,36 @@
+/*
+inspiration
+https://dribbble.com/shots/4684682-Aquatic-Animals
+*/
+
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 3,
+    slideShadows: true
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 1
+    },
+    1024: {
+      slidesPerView: 2
+    },
+    1560: {
+      slidesPerView: 3
+    }
+  }
+});
